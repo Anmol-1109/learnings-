49ec9eedccdda782634e21cd1d4125a0c17e9146
Anmol-1109 <anmolvats11.09@gmail.com>
2025-09-14 19:17:26 +0530

    just a change

--- enum.c
+++ enum.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 enum day{sun=1,mon=2,tue=3,wed=4,thu=5,fri=6,sat=7};
 int main(){
-printf("enter what day is today :");
+printf("enter what day is today in no :");
 int today;
 scanf("%d",&today);
-if(today==1||today==7){
+if(today==sun||today==sat){
     printf("holiday");
 }
 else{
