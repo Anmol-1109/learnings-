9352a2bbd6905fbe116c43062a0ef4dc0b4e005f
Anmol-1109 <anmolvats11.09@gmail.com>
2025-09-15 12:52:34 +0530

    files

---
+++ writing_files.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    FILE *pf=fopen("test.txt","w");
+    fprintf(pf,"hey I am Anmol");
+    FILE pf=fopen("test.txt","a");
+    fprintf(pf,"\nI am 19 years old");
+
+    fclose(pf);
+    /*if (remove("test.txt")==0){
+        printf("file deleted");
+    }
+    else{
+        printf("error");
+    }*/
+}
