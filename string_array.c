e8983ca4cda49b0e101cfbc416c22200441d9e7f
Anmol-1109 <anmolvats11.09@gmail.com>
2025-12-12 00:42:02 +0530

    string printing

--- string_array.c
+++ string_array.c
@@ -4,6 +4,12 @@ int main(){
     char cars[][10]={"bmw","audi","tesla"};
     strcpy(cars[1],"lambo");
     for(int i=0;i<sizeof(cars)/sizeof(cars[1]);i++){
-        printf("%s ",cars[i]);
-    }
+        printf("%s\n",cars[i]);
+        
 }
+char str[]="anmol sharma";
+for (int i=0;i<strlen(str);i++){
+ printf("%c",str[i]);
+}
+printf("%d",strlen(str));
+           }
