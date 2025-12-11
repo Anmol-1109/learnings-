a98aba6b60c7158b86bd79cd806502ad3a720b30
Anmol-1109 <anmolvats11.09@gmail.com>
2025-12-12 00:11:17 +0530

    another example

--- enum.c
+++ enum.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 enum day{sun=1,mon=2,tue=3,wed=4,thu=5,fri=6,sat=7};
 int main(){
-printf("enter what day is today in no :");
+/*printf("enter what day is today in no :");
 int today;
 scanf("%d",&today);
 if(today==sun||today==sat){
     printf("holiday");
 }
 else{
-    printf("go to work");
-}
+    printf("go to work");*/
+enum week{Mon, Tue, Wed, Thur, Fri, Sat, Sun};
+enum week day;
+day = Wed;
+printf("%d",day);
+return 0;
+
 }
