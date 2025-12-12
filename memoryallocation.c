a0cb17694767f88a07d23fefa601b27aab0d84db
Anmol-1109 <anmolvats11.09@gmail.com>
2025-12-13 02:28:19 +0530

    memory allocation

---
+++ memoryallocation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int * ptr;
+ptr=(int*)malloc(3*4);
+for(int i=0;i<3;i++){
+    scanf("%d",&ptr[i]);
+}
+for(int i=0;i<3;i++){
+    printf("%d\n",ptr[i]);
+}
+
+}
