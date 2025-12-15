35774ae61729dc0d05508814ddcbc137b62ef1f8
Anmol-1109 <anmolvats11.09@gmail.com>
2025-12-16 01:14:10 +0530

    writing and apending

--- reading_files.c
+++ reading_files.c
@@ -1,13 +1,27 @@
 #include<stdio.h>
 int main(){
+    //char buffer[255];
+    // if(fgets(buffer,255,pf)==NULL){
+    //     printf("error: unable to open file");
+    // }
+    // while(fgets(buffer,255,pf)!=NULL){
+    //     printf("%s",buffer);
+    // }
     FILE *pf= NULL;
     pf=fopen("New Text Document.txt","r");
-    char buffer[255];
-    if(fgets(buffer,255,pf)==NULL){
-        printf("error: unable to open file");
-    }
-    while(fgets(buffer,255,pf)!=NULL){
-        printf("%s",buffer);
-    }
-    fclose(pf);
+    char str[] = "well this is a example on how to write in files";
+    char string[100];
+    
+    fscanf(pf,"%s",string);
+    printf("%s",string);
+    
+    // fclose(pf);
+    // FILE *ptr= NULL;
+    // ptr=fopen("New Text Document.txt","W");
+    // fprintf(ptr,"%s",str);
+    // fclose(ptr);
+    FILE *ptr= NULL;
+    ptr=fopen("New Text Document.txt","a");
+    fprintf(ptr,"%s",str);
+    fclose(ptr);
 }
