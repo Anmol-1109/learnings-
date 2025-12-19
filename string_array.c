d19b52582dd920fde420498d22a0b4c486273bea
Anmol-1109 <anmolvats11.09@gmail.com>
2025-12-19 18:29:57 +0530

    accepting input

--- string_array.c
+++ string_array.c
@@ -8,9 +8,18 @@ int main(){
         
 }
 char str[]="anmol sharma";
+char string[45];
 for (int i=0;i<strlen(str);i++){
  printf("%c",str[i]);
 }
 printf("\n%s",str);
 //printf("%d",strlen(str));
+printf("\nenter the name ");
+scanf("%[^\n]s",string);
+scanf("%*c");
+printf("the name is %s",string);
+printf("\nenter the name ");
+fgets(string,45,stdin);
+printf("the name is %s",string);
+
            }
