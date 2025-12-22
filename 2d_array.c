8d1d1e86df7298e93b7ee6591860d11e90556b91
Anmol-1109 <anmolvats11.09@gmail.com>
2025-12-22 19:01:37 +0530

    transpose

--- 2d_array.c
+++ 2d_array.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+void print(int rows,int columns,int num[rows][columns]){
+     
+   
+     for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            printf("%d",num[i][j]);
+        }
+        printf("\n");
+    }
+}
+void atrans(int i, int j,int num[i][j]){
+int trans[j][i];
+for(int k=0;k<i;k++){
+    for(int l=0;l<j;l++){
+        trans[k][l]=num[l][k];
+
+    }
+}
+print(i,j,trans);
+
+
+}
 int main(){
     //int matrix[2][3]={1,2,3},{4,5,6,};//
        int num[3][3];
@@ -11,13 +33,10 @@ int main(){
        num[2][0]=7;
        num[2][1]=8;
        num[2][2]=9;
-       int rows=sizeof(num)/sizeof(num[0]);
+        int rows=sizeof(num)/sizeof(num[0]);
        int columns=sizeof(num[0])/sizeof(num[0][0]);
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-            printf("%d",num[i][j]);
-        }
-        printf("\n");
-    }
+       print(rows,columns,num);
+       atrans(rows,columns,num);
+     
 
 }
